815d20fe117cd4fe0000e102d8684906137b1887
eugene-bakulin <bakulin.eugene@gmail.com>
2022-05-02 02:10:51 +0400

    feat: add list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function removeKFromList(l, k) {
+
+  let head = l;
+
+  function countPositions(k) { // посчитаем, сколько раз нужно прогнать поиск позиции и удаление элемента
+      let current = l;
+      let positions = 0;
+
+      while (current) {
+          if (current.value === k) {
+              positions++;
+          }
+          current = current.next;
+      }
+      return positions;
+  }
+
+  let repeater = countPositions(k); // количество прогонок
+
+  function searchPosition(k) { // поиск позиции для удаления элемента
+      let current = head;
+      let position = 0;
+    
+      while (current) {
+        if (current.value === k) {
+          return position;
+        }
+    
+        current = current.next;
+        position++;
+      }
+      //return position;
+  }
+
+  function removeAt(position) {
+      let current = head;
+      if (position === 0) {
+        head = current.next;
+      } else {
+        let prev = null;
+        let index = 0;
+  
+        while (index < position) {
+          prev = current;
+          current = current.next;
+          index++;
+        }
+  
+        prev.next = current.next;
+      }
+  }
+    
+  for (let i=1; i <= repeater; i ++) {
+      let position = searchPosition(k);        
+      removeAt(position);
+  }
+
+  return head;
+ 
 }
 
 module.exports = {
